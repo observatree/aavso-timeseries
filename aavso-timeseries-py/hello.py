cf5979a76d44ad65256d33eaa69e9265a0234c7b
Brian Hill <brian@lingerhere.org>
2017-03-29 23:02:25 -0700

    Queries for distinct obscode.

--- aavso-timeseries-py/hello.py
+++ aavso-timeseries-py/hello.py
@@ -6,6 +6,17 @@ import mysql.connector
 # noinspection PyUnresolvedReferences
 cnx = mysql.connector.connect(user='leavitt', password=os.environ['MYSQL_PASSWORD'],
                               host='127.0.0.1',
-                              database='test')
+                              database='test')   # type: mysql.connector.connection.MySQLConnection
+
+cursor = cnx.cursor()
+
+query = "SELECT DISTINCT obscode FROM temp_observations"
+
+cursor.execute(query)
+
+for (observer_code) in cursor:
+    print("{}, is an obscode".format(observer_code))
+
 
 cnx.close()
+
