6dfa0ab031f5fc263296f6051efe75bb790a8727
Brian Hill <brian@lingerhere.org>
2017-04-01 17:52:39 -0700

    Made the record a named tuple.

--- aavso-timeseries-py/hello.py
+++ aavso-timeseries-py/hello.py
@@ -3,12 +3,18 @@ import os
 
 import mysql.connector
 
-from processing import identify_timeseries
+from processing import identify_timeseries, Observation
+
+USER = 'leavitt'
+HOST = '127.0.0.1'
+DATABASE = 'test'
+# So that the password does not appear in the code, the password is read from an environment variable.
+
 
 # noinspection PyUnresolvedReferences
-cnx_outer = mysql.connector.connect(user='leavitt', password=os.environ['MYSQL_PASSWORD'],
-                                    host='127.0.0.1',
-                                    database='test')   # type: mysql.connector.connection.MySQLConnection
+cnx_outer = mysql.connector.connect(user=USER, password=os.environ['MYSQL_PASSWORD'],
+                                    host=HOST,
+                                    database=DATABASE)   # type: mysql.connector.connection.MySQLConnection
 
 outer_cursor = cnx_outer.cursor()
 
@@ -17,22 +23,27 @@ outer_query = "SELECT DISTINCT name, obscode FROM temp_observations"
 outer_cursor.execute(outer_query)
 
 # noinspection PyUnresolvedReferences
-cnx_inner = mysql.connector.connect(user='leavitt', password=os.environ['MYSQL_PASSWORD'],
-                                    host='127.0.0.1',
-                                    database='test')   # type: mysql.connector.connection.MySQLConnection
+cnx_inner = mysql.connector.connect(user=USER, password=os.environ['MYSQL_PASSWORD'],
+                                    host=HOST,
+                                    database=DATABASE)   # type: mysql.connector.connection.MySQLConnection
 
 inner_cursor = cnx_inner.cursor()
 
 for (name, observer_code) in outer_cursor:
-    inner_query = "SELECT * FROM temp_observations WHERE name=\"{}\" AND obscode=\"{}\"".format(name, observer_code)
+    print("Processing {}'s observations of {}....".format(observer_code, name))
+    inner_query = ('SELECT unique_id, JD FROM temp_observations '
+                   'WHERE name="{}" AND obscode="{}"').format(name, observer_code)
     inner_cursor = cnx_inner.cursor()
     inner_cursor.execute(inner_query)
-    records = []
-    for record in inner_cursor:
-        records.append(record)
-        time_series = identify_timeseries(records)
+    observations = []
+    for (unique_id, julian_date) in inner_cursor:
+        observation = Observation(unique_id=unique_id,
+                                  julian_date_string=julian_date,
+                                  julian_date=None,
+                                  timeseries=None)
+        observations.append(observation)
 
-    print("Processed {}'s observations of {}".format(observer_code, name))
+    time_series = identify_timeseries(observations)
 
 
 cnx_inner.close()
--- aavso-timeseries-py/processing.py
+++ aavso-timeseries-py/processing.py
@@ -1,6 +1,17 @@
 
+import collections
+
+
+Observation = collections.namedtuple("Observation", "unique_id julian_date_string julian_date timeseries")
+
+
 # noinspection PyUnusedLocal
 
 
-def identify_timeseries(records: object):
-    return ()
+# It is presumed that identify_timeseries will be called with a list of records that are
+# all from the same observer and the same star. Therefore all that identify_timeseries has
+# to do is examine the Julian
+
+def identify_timeseries(observations: [Observation]):
+    print("{} observations".format(len(observations)))
+    return
