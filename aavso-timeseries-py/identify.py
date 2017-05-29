894d8aadfab04731f85189428a27b3a26207c39c
Brian Hill <brian@lingerhere.org>
2017-05-28 17:18:41 -0700

    Removed one-pass implementation on the grounds that having all records in memory is not as scalable as the two-pass implementation.

--- aavso-timeseries-py/identify.py
+++ aavso-timeseries-py/identify.py
@@ -91,86 +91,6 @@ def outer_processing_loop():
     print('{} sec'.format(time.time() - start_time))
 
 
-def processing_loop2():
-    # concept: single pass through the db. Maintain a table for each name+obscode combinations with the
-    #   the last obs. Write the timeseries determination for the last observation seen; put the current
-    #   one into the table. And finally write everything left in the table to the db.
-    start_time = time.time()
-    cnx_outer = mysql.connector.connect(user=USER,
-                                        password=PASSWORD,
-                                        host=HOST,
-                                        database=DATABASE)  # type: mysql.connector.connection.MySQLConnection
-    outer_cursor = cnx_outer.cursor(buffered=True)  # buffer so its not necessary to read the whole result
-    outer_query = "SELECT unique_id, JD, name, obscode FROM temp_observations order by JD"
-    outer_cursor.execute(outer_query)
-    print(outer_cursor.rowcount, ' %d sec' % (time.time() - start_time))  # how many cases to process?
-    cnt = 0
-
-    lastobs = dict()
-    # lastobsentry = collections.namedtuple("lastobsentry", "unique_id jd ts")
-    ind_id = 0
-    ind_jd = 1
-    ind_ts = 2
-    # set up to write the ts field
-    cnx = mysql.connector.connect(user=USER,
-                                  password=PASSWORD,
-                                  host=HOST,
-                                  database=DATABASE)  # type: mysql.connector.connection.MySQLConnection
-    write_cursor = cnx.cursor()
-
-    for (unique_id, JD, name, obscode) in outer_cursor:
-        jdf = float(JD)  # nb JD is varchar
-        h = name + obscode  # simplest hash
-        if h in lastobs:
-            if jdf - lastobs[h][ind_jd] < TIMESERIES_THRESHOLD:  # this obs is in a ts
-                if None is lastobs[h][ind_ts]:  # This is the start of the series
-                    lastobs[h][ind_ts] = lastobs[h][ind_id]  # ts is the id of the first obs
-                clearts = False
-            else:
-                clearts = True
-            # if we knew db had nulls in timeseries, could avoid writing the nulls over again
-            update_statement = ('UPDATE temp_observations SET timeseries2={} '
-                                'WHERE unique_id={}').format(lastobs[h][ind_ts], lastobs[h][ind_id])
-            # print(update_statement)
-            if lastobs[h][ind_ts]:
-                write_cursor.execute(update_statement)
-            #   this fails when trying to write None to the timeseries2 field. Needs to be null
-            #   this next line solves that problem, supposedly
-            #   but nothing is getting written.
-            #   update_statement = """UPDATE temp_observations SET timeseries2=%s WHERE unique_id=%s"""
-            #   write_cursor.execute(update_statement, lastobs[h][ind_ts], lastobs[h][ind_id])
-
-            lastobs[h][ind_id] = unique_id
-            lastobs[h][ind_jd] = jdf
-            if clearts:
-                lastobs[h][ind_ts] = None
-            # else leave the timeseries starting id in place
-
-        else:
-            lastobs[h] = [unique_id, jdf, None]
-        cnt += 1
-        if 0 == (cnt % 100000):
-            print(cnt, lastobs.__len__(), ' %d sec' % (time.time() - start_time))
-            cnx.commit()
-
-    print(cnt, lastobs.__len__(), ' %d sec' % (time.time() - start_time))
-    # clear the lastobs to the file
-    #    this doesn't work....
-    # for lo in lastobs:
-    #     update_statement = ('UPDATE temp_observations SET timeseries2={} '
-    #                         'WHERE unique_id={}').format(lo[ind_ts], lo[ind_id])
-    #     if (lo[ind_ts] != None):
-    #         write_cursor.execute(update_statement)
-
-    # wrap up write
-    write_cursor.close()
-    cnx.commit()
-    cnx.close()
-    # wrap up outer
-    outer_cursor.close()
-    cnx_outer.close()
-
-
 # I bracket script execution with this,
 
 # mysql> ALTER TABLE temp_observations DISABLE KEYS;
@@ -186,4 +106,3 @@ def processing_loop2():
 
 if __name__ == "__main__":
     outer_processing_loop()
-    # processing_loop2()
